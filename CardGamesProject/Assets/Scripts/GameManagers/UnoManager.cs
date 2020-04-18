50df6fd5070baa053ab4e593dd4a380a4adb4472
pcdrive <nova.terra94@gmail.com>
2020-04-18 21:02:58 +0200

    all uno card visuals added, scene updated

--- CardGamesProject/Assets/Scripts/GameManagers/UnoManager.cs
+++ CardGamesProject/Assets/Scripts/GameManagers/UnoManager.cs
@@ -13,32 +13,72 @@ public class UnoManager : GameManager {
     {
         instance = this;
 
-        GameObject card;
-        for (int i = 0; i < 10; i++)
+        int tabX = 0; // Cosmetics... =D
+        int tabZ = 0;
+        GameObject card; //This will hold the card objects until they are added to the list;
+        for (int n = 0; n < 2; n++)
         {
-            card = Instantiate(CardPrefab); // Creating a clone of the card prefab
-            card.transform.parent = transform; // Assigning the manager as its pareint in the scene hierarchy
-            card.transform.localPosition = new Vector3(0, 0, i * 10); // setting local position, relative to the manager
-            card.AddComponent<Card>(); // adding the card script to the object.
-            card.GetComponent<Card>().build("UI/UNO/Red_" + i, "UI/UNO/UNOBack", "Red_" + i); //Kártya build. UI-t állít be.
+            for (int j = 0; j < 8; j++)
+            {
+                string color;
+                string type;
+                for (int i = 0; i < 10; i++)
+                {
+                    color = "Red";
+                    if (j >= 2)
+                        color = "Blue";
+                    if (j >= 4)
+                        color = "Yellow";
+                    if (j >= 6)
+                        color = "Green";
 
-            card = Instantiate(CardPrefab); // Creating a clone of the card prefab
-            card.transform.parent = transform; // Assigning the manager as its pareint in the scene hierarchy
-            card.transform.localPosition = new Vector3(0, 0, i * 10 + 101); // setting local position, relative to the manager
-            card.AddComponent<Card>(); // adding the card script to the object.
-            card.GetComponent<Card>().build("UI/UNO/Blue_" + i, "UI/UNO/UNOBack", "Red_" + i); //Kártya build. UI-t állít be.
+                    card = Instantiate(CardPrefab); // Creating a clone of the card prefab
+                    card.transform.parent = transform; // Assigning the manager as its pareint in the scene hierarchy
+                    card.transform.localPosition = new Vector3(tabX * 100, 0, tabZ * 10); // setting local position, relative to the manager
+                    card.AddComponent<Card>(); // adding the card script to the object.
+                    card.GetComponent<Card>().build("UI/UNO/" + color + "_" + i, "UI/UNO/UNOBack", color + "_" + i); //Building the card. Sets the visuals.
+                    tabX++;
+                    tabZ++;
+                }
 
-            card = Instantiate(CardPrefab); // Creating a clone of the card prefab
-            card.transform.parent = transform; // Assigning the manager as its pareint in the scene hierarchy
-            card.transform.localPosition = new Vector3(0, 0, i * 10 + 201); // setting local position, relative to the manager
-            card.AddComponent<Card>(); // adding the card script to the object.
-            card.GetComponent<Card>().build("UI/UNO/Yellow_" + i, "UI/UNO/UNOBack", "Red_" + i); //Kártya build. UI-t állít be.
+                for (int i = 0; i < 3; i++)
+                {
+                    color = "Red";
+                    type = "X";
+                    if (j >= 2)
+                        color = "Blue";                        
+                    if (j >= 4)
+                        color = "Yellow";
+                    if (j >= 6)
+                        color = "Green";
+                    if (i == 1)
+                        type = "R";
+                    if (i == 2)
+                        type = "P2";
 
-            card = Instantiate(CardPrefab); // Creating a clone of the card prefab
-            card.transform.parent = transform; // Assigning the manager as its pareint in the scene hierarchy
-            card.transform.localPosition = new Vector3(0, 0, i * 10 + 301); // setting local position, relative to the manager
-            card.AddComponent<Card>(); // adding the card script to the object.
-            card.GetComponent<Card>().build("UI/UNO/Green_" + i, "UI/UNO/UNOBack", "Red_" + i); //Kártya build. UI-t állít be.
+                    card = Instantiate(CardPrefab); // Creating a clone of the card prefab
+                    card.transform.parent = transform; // Assigning the manager as its pareint in the scene hierarchy
+                    card.transform.localPosition = new Vector3(tabX * 100, 0, tabZ * 10);// setting local position, relative to the manager
+                    card.AddComponent<Card>(); // adding the card script to the object.
+                    card.GetComponent<Card>().build("UI/UNO/" + color + "_" + type, "UI/UNO/UNOBack", color + "_" + type); //Building the card. Sets the visuals.
+                    tabX++;
+                    tabZ++;
+                }
+            }
+            for (int j = 0; j < 4; j++)
+            {
+                string type = "Color";
+                if (j >= 2)
+                    type = "P4";
+
+                card = Instantiate(CardPrefab); // Creating a clone of the card prefab
+                card.transform.parent = transform; // Assigning the manager as its pareint in the scene hierarchy
+                card.transform.localPosition = new Vector3(tabX * 100, 0, tabZ * 10); // setting local position, relative to the manager
+                card.AddComponent<Card>(); // adding the card script to the object.
+                card.GetComponent<Card>().build("UI/UNO/" + type, "UI/UNO/UNOBack", type); //Building the card. Sets the visuals.
+                tabX++;
+                tabZ++;
+            }
         }
     }
 }
